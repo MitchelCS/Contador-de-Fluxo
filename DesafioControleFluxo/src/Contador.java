4f0260f92274285aad5101e0b23ee6fdef7c1105
Mitchel Camara Silva <168555075+MitchelCS@users.noreply.github.com>
2024-07-04 11:38:00 -0300

    Update Contador.java

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Contador {
 	public static void main(String[] args) {
+		//criar scanner
 		Scanner terminal = new Scanner(System.in);
 
 		System.out.println("Digite o primeiro parâmetro");
@@ -9,28 +10,29 @@ public class Contador {
 
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
-		
+
 		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);          
-		
-		}catch (ParametrosInvalidosException e) {
-            System.out.println(e.getMessage());			
+		// chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+
+		} catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());
 		}
-		
+
 	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametroDois <= parametroUm) {
-            throw new ParametrosInvalidosException(
-                "O segundo parâmetro deve ser maior que o primeiro");
-        }	
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		// validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroDois <= parametroUm) {
+			throw new ParametrosInvalidosException(
+					"O segundo parâmetro deve ser maior que o primeiro");
+		}
 
 		int contagem = parametroDois - parametroUm;
 
-		//realizar o for para imprimir os números com base na variável contagem
-        for (int i = 1; i <= contagem; i++) {
-            System.out.println("Imprimindo o número " + i);
-        }
-	} 
+		// realizar o for para imprimir os números com base na variável contagem
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+	}
 }
